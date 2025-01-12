5c989d11bbb182809144b2e66bb1ad48a743d5c9
Arthur Boi Paraja <arthur.ringoringo@gmail.com>
2025-01-12 19:04:06 +0700

    Update content_security_policy.rb

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,18 +7,18 @@
 Rails.application.configure do
   config.content_security_policy do |policy|
     policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https, :unsafe_inline
-    policy.style_src   :self, :https, :unsafe_inline
+    policy.font_src :self, :https
+    policy.img_src :self, :https, :data # data may be unnecessary for your use case.
+    policy.object_src :none
+    policy.script_src :self, :https
+    policy.style_src :self, :https
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
 
   # Generate session nonces for permitted importmap and inline scripts
   config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  config.content_security_policy_nonce_directives = %w(script-src)
+  config.content_security_policy_nonce_directives = %w[script-src style-src]
 
   # Report violations without enforcing the policy.
   # config.content_security_policy_report_only = true
