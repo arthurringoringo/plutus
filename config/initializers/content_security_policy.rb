b88985e76df2f3b33cf401c52dcdcfaa8e638650
Arthur Boi Paraja <arthur.ringoringo@gmail.com>
2025-01-12 18:24:16 +0700

    csp

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -10,8 +10,8 @@ Rails.application.configure do
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
+    policy.script_src  :self, :https, -> { "'nonce-#{request.content_security_policy_nonce}'" }
+    policy.style_src   :self, :https, :unsafe_inline
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
