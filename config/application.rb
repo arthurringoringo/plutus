e57776a22aebeb179ea5d0293b13be2c1dc9558c
Arthur Boi Paraja <arthur.ringoringo@gmail.com>
2025-01-26 14:18:53 +0700

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Plutus
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    config.action_dispatch.default_headers = {
+      'Content-Security-Policy' => "script-src 'self' 'unsafe-inline' https:"
+    }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
